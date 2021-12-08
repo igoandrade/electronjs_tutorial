c9530a1f58983b75a3ca7b84e981ac1bd2f2e04b
Igo da Costa Andrade <igoandrade.314@gmail.com>
2021-12-08 19:10:48 +0000

    Teste de uso do electron no gitpod

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM gitpod/workspace-full-vnc
+RUN apt-get update \
+    && apt-get install -y libnss
---
+++ project-01/hello-app/index.js
@@ -0,0 +1,20 @@
+// 1. Import electron objects
+const { app, BrowserWindow } = require('electron');
+
+
+// 2. Reserve a reference to window object
+let window;
+
+// 3. Wait till application started
+app.on('ready', () => {
+    window = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    // 5. Load window content
+    window.loadFile('index.html');
+});
